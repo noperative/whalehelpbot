4241a242b325632aac676fff1fddebd57367a89f
Noperative <ruimoutrickster@hotmail.com>
2016-03-01 17:02:40 -0800

    adding replies

---
+++ replybuilder.py
@@ -0,0 +1,24 @@
+
+base_string = """[TE ~ I ~ TO ~ KU~ TEITOKU!](http://nyoron.co/TAIGEI.ogg) Looks like you have some questions.
+I may be a bot, but I am glad to be of service!
+
+-----
+"""
+
+start_string = """"-----
+It looks like you want some help in registering for the game.
+Kancolle is a region-locked game so you will need to fiddle around with your settings
+or use some safe, third-party programs to streamline the process.
+[Check the reddit wiki page on the process](https://www.reddit.com/r/kancolle/wiki/gettingstarted)
+"""
+
+beginner = """-----
+Do you want some help with the basics?
+You can find a short tutorial of the game mechanics on [this wiki page](https://www.reddit.com/r/kancolle/wiki/beginner).
+"""
+
+
+end_string = """-----
+^(*I-I-I'm actually a bot! Please contact my owner /u/Noperative if you have any questions.*)
+
+"""

a8985baded1cf0e5e835129d58ce9614de50f2b8
Noperative <ruimoutrickster@hotmail.com>
2016-03-09 12:09:10 -0800

    fixed tab errors and more consistency issues with post/reply to submission/comment

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ import os
 r_instance = None
 already_commented = []
 
+
 def load_already_commented():
-    with open("commented.txt", "r+") as file:
-	already_commented_ids = [i for i in file.readlines()]
-	#cleaning code that may be used one day
-	if len(already_commented_ids) > 10000:
-	   already_commented_ids = already_commented_ids[-10000:]
-	   file.seek(0)
-	   file.writelines(already_commented_ids)
-    return already_commented_ids
+    with open("commented.txt", "r+") as f:
+        already_commented_ids = [i for i in f.readlines()]
+    # cleaning code that may be used one day
+    if len(already_commented_ids) > 10000:
+        already_commented_ids = already_commented_ids[-10000:]
+        f.seek(0)
+        f.writelines(already_commented_ids)
+        return already_commented_ids
+
 
 def username_mentions():
     mentions = r_instance.get_mentions(limit=10)
@@ -26,24 +28,26 @@ def username_mentions():
     for u in unreads:
         for m in mentions:
             if m == u:
-               print "mentioned by: " + m.id + " for " + m.comment.submission.id
-               u.mark_as_read()
-               build_comment(m.comment.submission)
+                print "mentioned by: " + m.id + " for " + m.comment.submission.id
+                u.mark_as_read()
+                build_comment(m.comment.submission)
+
 
 def scan_submission(submission):
     print "scanning submission: " + submission.title + " " + submission.id
     if '[help]' in submission.title.lower():
         if any(word in submission.selftext.lower for word in properties.general_words):
-            build_reply(submission)  
+            build_comment(submission)
+
 
 def build_comment(submission):
     global already_commented
     if submission.id not in already_commented:
-        already_commented.append(post.id)
-	with open("commented.txt", "a") as file:
-	   file.write('{0!s}\n'.format(submission.id))
-    	comment = "taigei"
-    	submission.add_comment(comment)
+        already_commented.append(submission.id)
+    with open("commented.txt", "a") as f:
+        f.write('{0!s}\n'.format(submission.id))
+    comment = "taigei"
+    submission.add_comment(comment)
 
 def main():
     global r_instance
@@ -66,10 +70,9 @@ def main():
     while True:
         oauth_instance.refresh(force=True)
         for i in range(0,60):
-            #username_mentions()
+            # username_mentions()
             for submission in sub_r.get_new(limit=3):
-		if submission.id not in already_commented:
-                  scan_submission(submission)
+                scan_submission(submission)
             sleep(59)
     
 

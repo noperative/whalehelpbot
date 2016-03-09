5940477f05ad5c4d7e2f78bb5de5101bf876eed4
Noperative <ruimoutrickster@hotmail.com>
2016-03-09 11:46:34 -0800

    rewrote post/reply -> submission/comment for consistency, bot should be able to reply to comments now

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ r_instance = None
 already_commented = []
 
 def load_already_commented():
-    with open("commented.txt", "rw") as file:
-	already_commented = [i for i in file.readlines()]
+    with open("commented.txt", "r+") as file:
+	already_commented_ids = [i for i in file.readlines()]
 	#cleaning code that may be used one day
-	if len(already_commented) > 10000:
-	   already_commented = already_commented[-10000:]
-	   file.writelines(already_commented)
-    return already_commented
+	if len(already_commented_ids) > 10000:
+	   already_commented_ids = already_commented_ids[-10000:]
+	   file.seek(0)
+	   file.writelines(already_commented_ids)
+    return already_commented_ids
 
 def username_mentions():
     mentions = r_instance.get_mentions(limit=10)
@@ -25,24 +26,24 @@ def username_mentions():
     for u in unreads:
         for m in mentions:
             if m == u:
-                print "mentioned by: " + m.id " for " + m.comment.submission.id
-                u.mark_as_read()
-                build_comment(m.comment.submission)
-
-
+               print "mentioned by: " + m.id " for " + m.comment.submission.id
+               u.mark_as_read()
+               build_comment(m.comment.submission)
 
 def scan_submission(submission):
     print "scanning submission: " + submission.title + " " + submission.id
-    if '[help]' in post.title.lower():
-        if any(word in post.selftext.lower for word in models.general_words):
-            build_reply(post)  
+    if '[help]' in submission.title.lower():
+        if any(word in submission.selftext.lower for word in properties.general_words):
+            build_reply(submission)  
 
 def build_comment(submission):
     global already_replied
     if submission.id not in already_replied:
         already_replied.append(post.id)
-    reply = "taigei"
-    post.add_comment(reply)
+	with open("commented.txt", "a") as file:
+	   file.write('{0!s}\n'.format(submission.id)
+    	reply = "taigei"
+    	submission.add_comment(reply)
 
 def main():
     global r_instance

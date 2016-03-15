d124bdf3856feba45186c68d8c193c99afd8ccdd
Noperative <ruimoutrickster@hotmail.com>
2016-03-15 15:50:22 -0700

    planning out matrix calculations

--- matrixbuilder.py
+++ matrixbuilder.py
@@ -1,3 +1,8 @@
 import numpy as np
 import pandas as pd
 
+d = {'joining': pd.Series([1., 2., 3.], index=['a', 'b', 'c']),
+     'tutorial': pd.Series([10, 2., 3., 4.], index=['beginner_tutorial', 'b', 'c', 'd'])}
+
+df = pd.DataFrame(d)
+
